d3821280c2b2fedaae280fe67fc2146cce4ccba5
Rahul10182 <rahulkumarsingh3226@gmail.com>
2024-11-07 21:06:38 +0530

    Changes in models

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -14,10 +14,6 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Review' 
     }],
-    location: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Location' 
-    },
     profile: {
         bio: { type: String },
         phoneNumber: { type: Number },
